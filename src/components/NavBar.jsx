3889ffb6858ea52e5dcfa71f501cf4b52520b877
Sachintha Hasaranga <99173290+sachinthahasaranga@users.noreply.github.com>
2024-09-30 01:36:41 +0530

    create NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';  
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <Link className="navbar-brand" to="/">
+                    <img src="/images/logo.png" alt="Sparkhause Logo" style={{ height: '80px' }} />
+                </Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/">Home <span className="sr-only"></span></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/services">Services</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/blogs">Blogs</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
