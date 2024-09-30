b1af0a47b4dcad229ff6f2d429623be0a391e465
Sachintha Hasaranga <99173290+sachinthahasaranga@users.noreply.github.com>
2024-10-01 00:46:18 +0530

    add function to navigate to blog section from pagination V1.6.7

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
     const pageNumbers = [];
+    const navigate = useNavigate();
+    const location = useLocation(); 
 
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
         pageNumbers.push(i);
     }
+
+    const handlePaginationClick = (number) => {
+        paginate(number);
+        
+        if (location.pathname === '/') {
+            navigate("/");
+            setTimeout(() => {
+                const section = document.getElementById('blog-section');
+                if (section) {
+                    section.scrollIntoView({ behavior: 'smooth' });
+                }
+            }, 100);
+        }
+    };
 
     return (
         <nav>
             <ul className="pagination justify-content-end">
                 {pageNumbers.map(number => (
                     <li key={number} className="page-item">
-                        <a onClick={() => paginate(number)}  className="page-link">
+                        <a onClick={() => handlePaginationClick(number)} className="page-link">
                             {number}
                         </a>
                     </li>
@@ -23,3 +40,4 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
 };
 
 export default Pagination;
+
